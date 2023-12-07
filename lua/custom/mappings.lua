0147b5f3d2a82a4fdff3760c76ba6db76c353b1a
Hao Sun <hsun-code@outlook.com>
2023-12-07 04:19:47 +0000

    mapping: add telescope fc and fr

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -11,5 +11,14 @@ M.general = {
 }
 
 -- more keybinds!
+M.telescope = {
+  -- ref: https://github.com/nvim-telescope/telescope.nvim
+  n = {
+    -- find
+    ["<leader>fc"] = { "<cmd> Telescope grep_string <CR>", "Grep current cursor" },
+    ["<leader>fr"] = { "<cmd> Telescope resume <CR>", "Resum back" },
+    -- strict match? \bYour-word\b
+  }
+}
 
 return M
